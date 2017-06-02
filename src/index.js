b81eee6cec19ce9a22882e0943e99e2ecc7a7f7d
lizdenhup <lizdenhup@gmail.com>
2017-06-02 15:41:40 -0500

    imported uikit for css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './styles/uikit.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />, document.getElementById('root'));
 registerServiceWorker();
