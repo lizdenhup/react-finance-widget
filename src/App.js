e1dc2f275accad97a4b20909f749015b1f2bc0a7
lizdenhup <lizdenhup@gmail.com>
2017-06-05 13:49:54 -0500

    need to add uikit back

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
+import { StyleSheet, css} from 'aphrodite';
+
+const styles = StyleSheet.create({
+  navbar: {
+    width: '100%',
+    color: 'white',
+    background: '#18121e',
+    padding: '6px',
+    height: '40px'
+  }
+})
+
+const Signup = () => <div>Signup</div>
+const Home = () => <div>Home</div>
+const NotFound = () => <div>NotFound</div>
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="navbar">
-          <a href="/signup">Signup</a>
+      <Router>
+        <div className="App">
+          <div className="navbar">
+          
+          </div>
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+            <Route component={NotFound} />
+          </Switch>
         </div>
-      </div>
+      </Router>
     );
   }
 }
