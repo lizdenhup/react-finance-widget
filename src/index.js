757b6d0258f3234e552c96667e39b48d4a51e164
lizdenhup <lizdenhup@gmail.com>
2017-06-05 16:29:23 -0500

    reorganized files to use redux generator

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import configureStore from './store/configure-store';
+
+const store = configureStore();
 import './styles/uikit.css'
 
-ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>,
   <App />, document.getElementById('root'));
 registerServiceWorker();
--- src/views/Signup/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Signup = () => <form className="signup_form">Signup</form>
-
-export default Signup; 
