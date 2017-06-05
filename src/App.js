aafa0fc49bf906e346722fa5ed06e4eb27176705
lizdenhup <lizdenhup@gmail.com>
2017-06-05 16:52:26 -0500

    absolute mess resulting from garbage generator I found on npm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
-
   Route,
   Switch,
   Link
--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {Provider} from 'react-redux';
 import configureStore from './store/configure-store';
-const store = configureStore();
-
 import './styles/uikit.css'
 
+const store = configureStore();
+
+ReactDOM.render (
 <Provider store={store}>
   <App />
-    </Provider>,
-  <App />, document.getElementById('root'));
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
