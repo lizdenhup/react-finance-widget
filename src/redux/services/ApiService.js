a8523ebe5a8b0db016544a2d263fa52700905b7d
lizdenhup <lizdenhup@gmail.com>
2017-06-06 15:14:31 -0500

    added get, post, patch, delete handlers to ApiService

--- src/redux/services/ApiService.js
+++ src/redux/services/ApiService.js
@@ -1,3 +1,61 @@
 import fetch from 'isomorphic-fetch'
 
 const BASE_URL = 'https://pacific-plains-89545.herokuapp.com/api/v1'
+
+const token = JSON.parse(localStorage.getItem('token'))
+ 
+const headers =  {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer: ${token}`
+}
+
+const parseResponse = (response) => {
+ return response.json()
+   .then(json) => {
+     if (!response.ok) {
+       return Promise.reject(json.errors)
+     }
+
+     return json
+   }
+}
+
+export default {
+  
+  get(url) {
+    return fetch(`${BASE_URL}${url}`, {
+      method: 'GET',
+      headers: 'headers'
+    })
+    .then(parseResponse)
+  },
+
+  post(url, data ={}) {
+    const body = JSON.stringify(data)
+    return fetch(`${BASE_URL}${url}`, {
+      method: 'POST',
+      headers: 'headers',
+      body: body
+    })
+    .then(parseResponse)
+  },
+
+  patch(url, data ={}) {
+    const body = JSON.stringify(data)
+    return fetch(`${BASE_URL}${url}`, {
+      method: 'PATCH',
+      headers: 'headers',
+      body: body
+    })
+    .then(parseResponse)
+  },
+
+  delete(url) {
+    return fetch(`${BASE_URL}${url}`, {
+      method: 'DELETE',
+      headers: 'headers'
+    })
+    .then(parseResponse)
+  }
+}
