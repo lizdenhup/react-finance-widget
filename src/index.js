d0d7dcc33a973639b1165f44b485c66e96db3d6c
lizdenhup <lizdenhup@gmail.com>
2017-06-06 10:38:47 -0500

    removed all code generated by the react generator I found

--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {Provider} from 'react-redux';
-import configureStore from './store/configure-store';
 import './styles/uikit.css'
 
-const store = configureStore();
-
 ReactDOM.render (
-<Provider store={store}>
-  <App />
-</Provider>, document.getElementById('root'));
+  <App />, document.getElementById('root'));
 registerServiceWorker();
