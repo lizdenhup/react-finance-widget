3880c0940d658f4595aaa79e8dedf03b5e08f82d
lizdenhup <lizdenhup@gmail.com>
2017-06-06 15:50:54 -0500

    added functions to handle form input for signup/login

--- src/views/Forms/user.js
+++ src/views/Forms/user.js
@@ -1,20 +1,86 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const UserForm = (props) =>
+// front-end validations for form submitted data, not perfect 
+const validate = (values) => {
+  const errors = {};
+  if (!values.email) {
+    errors.email = 'A valid email is required';
+  } 
+  if (!values.password) {
+    errors.password = 'A password is required';
+  } else if (values.password.length < 8) {
+    errors.password = 'Password must be at least 8 characters';
+  }
+  return errors;
+}
 
-    <form className="uk-form-stacked">
+//updated syntax to use es6 class
+class UserForm extends Component {
 
-      <div className="uk-margin">
-          <label className="uk-form-label" htmlFor="email">Email</label>
-          <div className="uk-form-controls">
-              <input className="uk-input uk-form-width-medium" id="email" type="text" placeholder="Email" />
-          </div><br />
-        <label className="uk-form-label" htmlFor="password">Password</label>
-          <div className="uk-form-controls">
-              <input className="uk-input uk-form-width-medium" id="password" type="password" placeholder="Password" />
-          </div><br />
-        <input type="submit" className="uk-button uk-button-default" value={props.action === "signup" ? "Create User" : "Log In"} />
-      </div>
-    </form>
+  constructor(props) {
+    super(props)
 
-export default UserForm;
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  handleSubmit = (data) => this.props.onSubmit(data)
+
+  handleChange(event) {
+    if (event.target.name === 'email') {
+      this.setState({
+        email: event.target.value
+      })
+    } else if (event.target.name === 'password') {
+      this.setState({
+        password: event.target.value
+      })
+    }
+  }
+
+  render() {
+    const {handleSubmit} = this.props
+    return (
+      <form className="uk-form-stacked" onSubmit={handleSubmit(this.handleSubmit)}>
+
+        <div className="uk-margin">
+            <label className="uk-form-label" htmlFor="email">Email</label>
+            <div className="uk-form-controls">
+              <Field
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange.bind(this)}
+                className="uk-input uk-form-width-medium"
+                component="input"
+                id="email"
+                type="text"
+                placeholder="Email"
+              />
+            </div><br />
+          <label className="uk-form-label" htmlFor="password">Password</label>
+            <div className="uk-form-controls">
+              <Field
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange.bind(this)}
+                className="uk-input uk-form-width-medium"
+                component="input"
+                id="password"
+                type="password"
+                placeholder="Password"
+              />
+            </div><br />
+          <input type="submit" className="uk-button uk-button-default" value={this.props.action === "signup" ? "Create User" : "Log In"} />
+        </div>
+      </form>
+    )
+  }
+}
+
+export default reduxForm({
+  form: 'user',
+  validate
+})(UserForm);
