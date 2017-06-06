272f43cae4c89f9064afd50d0efb2c64c54a5aed
lizdenhup <lizdenhup@gmail.com>
2017-06-06 10:50:29 -0500

    added views directory and welcome view

---
+++ src/views/Welcome/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const  Welcome = () =>
+    <div className="uk-position-center">
+      <h1 className="uk-heading-line uk-text-center"><span>Finance Widget allows you to watch your favorite stocks over time</span></h1>
+      <h3 className="uk-heading-line uk-text-center"><span>Please <a className="uk-link-muted" href='/signup'>sign up</a> or <a className="uk-link-muted" href='/login'>log in</a> to use</span></h3>
+    </div>
+
+export default Welcome
