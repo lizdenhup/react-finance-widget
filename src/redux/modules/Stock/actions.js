a1e39627ef532179b394597c74c3b086e20deb50
lizdenhup <lizdenhup@gmail.com>
2017-06-07 17:02:25 -0500

    adding stock actions

--- src/redux/modules/Stock/actions.js
+++ src/redux/modules/Stock/actions.js
@@ -1,6 +1,19 @@
 import ApiService from '../../services/ApiService'
 
 //actions
+const stockRequest = () => {
+  return {
+    type: 'STOCK_REQUEST'
+  }
+}
+
+const stockRequestSuccess = () => {
+  return {
+    type: 'STOCK_REQUEST_SUCCESS',
+    stock: stock 
+  }
+}
+
 
 // aync function calls
 // GET stock
