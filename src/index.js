ed8fdaa89a7ffce2e81f08fd791576046d320cb9
lizdenhup <lizdenhup@gmail.com>
2017-06-07 18:15:48 -0500

    removed unused connect from index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider, connect} from 'react-redux'
+import { Provider } from 'react-redux'
 import store from './redux/store'
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
