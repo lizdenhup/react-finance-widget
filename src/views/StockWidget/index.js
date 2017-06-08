2e7cff697cdb92c40fd3575ce7c79ecdbb34fe24
lizdenhup <lizdenhup@gmail.com>
2017-06-08 10:46:06 -0500

    defined bare StockWidget component

---
+++ src/views/StockWidget/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class StockWidget extends Component {
+  render() {
+    return (
+      <div> 
+        {/* render some tables or charts of stocks whose data is fetched in the stock container */}
+      </div>
+    );
+  }
+}
+  
+  // have this connect to stocks array in your state
+
+export default StockWidget;
