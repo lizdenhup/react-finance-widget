0f1f54b4081e4bc364da28d316ed203c4120fe61
lizdenhup <lizdenhup@gmail.com>
2017-06-08 10:01:33 -0500

    change dashboard to become a container for a predefined array of stocks

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
-import StockForm from '../../views/components/Forms/stock';
 
 class Dashboard extends Component {
-
-  makeStockRequest = data => this.props.youraction(data)
-
   render() {
     return (
-      <div className="uk-position-center">
-        <StockForm onSubmit={this.props.makeStockRequest} />
-        {/* stock list component */}
+      <div> 
+        {/* render some tables or charts of a predefined set of stocks */}
       </div>
     );
   }
 }
   
-
   // have this connect to stocks array in your state
 
 export default Dashboard;
