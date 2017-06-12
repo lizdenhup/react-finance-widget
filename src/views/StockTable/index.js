dffdd822f8fbb6d3d97bdbd4aa8bab37a07c3e11
lizdenhup <lizdenhup@gmail.com>
2017-06-12 14:35:07 -0500

    removed earlier table effort

--- src/views/StockTable/index.js
+++ src/views/StockTable/index.js
@@ -1,37 +1,16 @@
 import React, { Component } from 'react';
-// import DATA from '../../redux/modules/Stock/data';
+import Table from 'react-uikit-table';
+import sampleData from '../../redux/modules/Stock/data';
 
-
-class StockWidget extends Component {
+class StockTable extends Component {
   render() {
     return (
       <div> 
-        {/*<table class="uk-table uk-width-1-1">
-            <caption>Data for {DATA['Meta Data']['2. Symbol']} as of {Object.keys(DATA['Time Series (Daily)'])[0]}</caption>
-            <thead>
-                <tr>
-                    <th>Open</th>
-                    <th>High</th>
-                    <th>Low</th>
-                    <th>Close</th>
-                    <th>Volume</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td class="uk-width-1-1 uk-text-right">{Object.values(DATA['Time Series (Daily)'])[0]['1. open']}</td>
-                    <td class="uk-width-1-1 uk-text-right">{Object.values(DATA['Time Series (Daily)'])[0]['2. high']}</td>
-                    <td class="uk-width-1-1 uk-text-right">{Object.values(DATA['Time Series (Daily)'])[0]['3. low']}</td>
-                    <td class="uk-width-1-1 uk-text-right">{Object.values(DATA['Time Series (Daily)'])[0]['4. close']}</td>
-                    <td class="uk-width-1-1 uk-text-right">{Object.values(DATA['Time Series (Daily)'])[0]['5. volume']}</td>
-                </tr>
-            </tbody>
-        </table>*/}
+
+        <Table caption='Generated table.' head={['Heading', 'Heading', 'Heading']} body={sampleData}/>
       </div>
     );
   }
 }
 
-  // have this connect to stocks array in your state
-
-export default StockWidget;
+export default StockTable;
