aea94c2d5babbf4df406e6b84b93eb5232c5d10f
lizdenhup <lizdenhup@gmail.com>
2017-06-12 16:18:54 -0500

    commented out uneeded const

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import StockTable from '../StockTable';
 import { connect } from 'react-redux';
 import { stockRequest, stockRequestSuccess, search } from '../../redux/modules/Stock/actions'
+
 class Dashboard extends Component {
 
 componentDidMount() {
@@ -17,10 +18,9 @@ componentDidMount() {
   }
 }
 
-
 export default connect(
   state => ({
-    stocks: state.stocks,
+    stocks: state.stock.stocks,
   }), {
     stockRequest,
     stockRequestSuccess,
