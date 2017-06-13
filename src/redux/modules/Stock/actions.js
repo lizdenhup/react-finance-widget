672c56735d596e999da33f78b63c413450c90b79
lizdenhup <lizdenhup@gmail.com>
2017-06-13 10:32:30 -0500

    hardcoded AMZN stock symbol into get request for testing purposes, removed uneeded files

--- src/redux/modules/Stock/actions.js
+++ src/redux/modules/Stock/actions.js
@@ -2,24 +2,56 @@ import ApiService from '../../services/ApiService';
 import { reset } from 'redux-form';
 
 //actions
-export const stockRequest = () => {
+export const getStock = () => {
   return {
-    type: 'STOCK_REQUEST'
+    type: 'GET_STOCK'
   }
 }
 
-export const stockRequestSuccess = (stock) => {
+export const getStockPending = () => {
   return {
-    type: 'STOCK_REQUEST_SUCCESS',
+    type: 'GET_STOCK_PENDING'
+  }
+}
+
+export const getStockFulfilled = (stock) => {
+  return {
+    type: 'GET_STOCK_FULFILLED',
     stock: stock 
   }
 }
 
-export const stockRequestFailure = () => {
-
+export const getStockRejected = () => {
+  return {
+    type: 'GET_STOCK_REJECTED'
+  }
 }
 
 // async function calls
+
+export function get_stock() {  
+  const action_type = "GET_STOCK";
+  return dispatch => {
+    // Dispatch the action for telling our reducer 
+    // that the API call is in progress
+    dispatch({type: `${action_type}_PENDING`});
+    ApiService.get(`/search?query=AMZN`)
+      .then(resp => {
+        // Dispatch the success action with the payload
+        dispatch({
+          type: `${action_type}_FULFILLED`,
+          payload: resp.data.results
+        });
+      })
+      .catch(err => {
+        // Dispatch the error action with error information
+        dispatch({
+          type: `${action_type}_REJECTED`,
+          error: err
+        });
+      });
+  };
+}
 
 
 
