edc7d0e6506b314ab8bc7f0da5c2df87076728e9
lizdenhup <lizdenhup@gmail.com>
2017-06-13 10:40:00 -0500

    added presentational components to get site to compile

---
+++ src/views/Dashboard/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const  Dashboard = () =>
+    <div className="uk-position-center">
+        <h2>In progresss...</h2>
+    </div>
+
+export default Dashboard
---
+++ src/views/Search/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const  Search = () =>
+    <div className="uk-position-center">
+        <h2>In progresss...</h2>
+    </div>
+
+export default Search 
