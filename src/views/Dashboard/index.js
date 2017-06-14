17c29af8005b82302532d74546c9a54a512b9fa1
lizdenhup <lizdenhup@gmail.com>
2017-06-13 21:12:54 -0500

    cleared chrome cache and api service is working again

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -11,7 +11,7 @@ class Dashboard extends Component {
     render() {
         return (
             <div className="uk-position-center">
-               
+             
             </div>
             )
         }
