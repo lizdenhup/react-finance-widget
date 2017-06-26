4747c302137d107e5cdcb8e1a4dc9691dc71cf3b
lizdenhup <lizdenhup@gmail.com>
2017-06-26 10:33:37 -0500

    setting up search form

--- src/views/Search/index.js
+++ src/views/Search/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { getStock } from '../../redux/modules/Stock/actions';
 import StockForm from '../components/Forms/stock';
 
-const  Search = () =>
-    <div className="uk-position-center">
-        <StockForm />
-    </div>
+class Search extends Component {
 
-export default Search 
+//   handleSearch = data => this.props.getStock({stockSymbol: data})
+
+  render() {
+    return(
+      <div className="uk-position-center">
+        <h2 className="uk-heading-line uk-text-center"><span>Enter a ticker symbol:</span></h2>
+        {/*<StockForm action="search" onSubmit={this.handleSearch} />*/}
+      </div>
+      )
+  }
+}
+
+
+export default connect(null, { getStock })(Search)
