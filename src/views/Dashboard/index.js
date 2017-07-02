ec4d697bd5c287c1f9e5912234d78d4ab696d527
lizdenhup <lizdenhup@gmail.com>
2017-07-02 17:27:13 -0500

    getting logo thumbnail to display while data is being fetched

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { fetchStocksWithRedux } from 
 '../../redux/modules/Stock/actions';
 import  { connect } from 'react-redux';
+import { logo } from '../../logo.svg'
 import Table from 'react-uikit-table';
 
 class Dashboard extends Component {
@@ -11,6 +12,7 @@ class Dashboard extends Component {
     }
 
     render() {
+        const { inProgress } = this.props 
         const stockObj = [Object.values(this.props.stockData)[1]][0]
         var todaysData = []   
         for (var date in stockObj) {
@@ -23,15 +25,20 @@ class Dashboard extends Component {
         }    
         const tableData = [{d1: todaysData[0], d2: todaysData[1], d3: todaysData[2], d4: todaysData[3], d5: todaysData[4]}]
         return (
-            <div className="uk-position-center">
+        <div className="uk-position-center">
+         {
+            inProgress ? <img src={logo} className="App-logo" alt="logo" />
+            :
             <Table caption="Today's Stock Data for AAPL" head={['Open', 'High', 'Low', 'Close', 'Volume']} body={tableData}/>
-            </div>
-            )
-        }
+            }
+        </div>
+        )
     }
+}
 
 export default connect(
   state => ({
+    inProgress: state.stock.inProgress,
     stocks: state.stocks,
     stockData: state.stock.stockData
   })
