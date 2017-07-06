a0ad3b2a4dbea27fab8f1f787b6aa2df53e3b32e
lizdenhup <lizdenhup@gmail.com>
2017-07-06 10:38:48 -0500

    adding reducer fns

--- src/redux/modules/Stock/reducer.js
+++ src/redux/modules/Stock/reducer.js
@@ -1,10 +1,16 @@
 const initialState = {
-  inProgress: false
+  inProgress: false,
+  stocks: []
 }
 
 export default (state = initialState, action) => {
   switch(action.type) {
-    
+
+    case 'ADD_STOCK':
+    return {
+      stocks: state.stocks.concat(action.stock)
+    }
+
     default: 
       return state;
   }
