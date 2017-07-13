a15987a84e95aff7f01be01622d367f4248f7cc9
lizdenhup <lizdenhup@gmail.com>
2017-07-13 16:14:05 -0500

    working search with interpolated ticker name

--- src/views/Search/index.js
+++ src/views/Search/index.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { search, updateStockTicker } from '../../redux/modules/Stock/actions';
 import logo from '../../logo.svg';
 import '../../styles/spin.css';
-import Table from 'react-uikit-table';
+import StockTable from '../StockTable'
 
 class Search extends Component {
 
@@ -21,9 +21,6 @@ class Search extends Component {
   }
 
   render() {
-    // if this.props.stockSymbol !== "" && Object.keys(this.props.stockData) !== 0
-    // render result
-    // else, render search form
     if (this.props.isFetchingData) {
       return (
         <div className="uk-position-center">
@@ -31,22 +28,9 @@ class Search extends Component {
         </div>
       )
     } else if (this.props.hasFetchedData) {
-      const ticker = this.props.stockData['Meta Data']['2. Symbol']
-      const stockObj = [Object.values(this.props.stockData)[1]][0]
-      var todaysData = []   
-        for (var date in stockObj) {
-            todaysData.push(`${stockObj[date]['1. open']}`);
-            todaysData.push(`${stockObj[date]['2. high']}`);
-            todaysData.push(`${stockObj[date]['3. low']}`);
-            todaysData.push(`${stockObj[date]['4. close']}`);
-            todaysData.push(`${stockObj[date]['5. volume']}`);
-            break;
-        }   
-      const tableData = [{d1: todaysData[0], d2: todaysData[1], d3: todaysData[2], d4: todaysData[3], d5: todaysData[4]}]
       return (
       <div className="uk-position-center">
-      {ticker}
-      <Table caption="Today's Stock Data for {ticker}" head={['Open', 'High', 'Low', 'Close', 'Volume']} body={tableData}/>
+      <StockTable /> 
       </div> 
       )
     } else {
@@ -66,6 +50,7 @@ class Search extends Component {
     } 
   }
 }
+// need to pass state to props for any component from redux store
 
 function mapStateToProps(state) {
   return {
---
+++ src/views/StockTable/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import Table from 'react-uikit-table';
+
+class StockTable extends Component {
+
+  render() {
+    const ticker = this.props.stockData['Meta Data']['2. Symbol']
+    const stockObj = [Object.values(this.props.stockData)[1]][0]
+    var todaysData = []   
+    for (var date in stockObj) {
+        todaysData.push(`${stockObj[date]['1. open']}`);
+        todaysData.push(`${stockObj[date]['2. high']}`);
+        todaysData.push(`${stockObj[date]['3. low']}`);
+        todaysData.push(`${stockObj[date]['4. close']}`);
+        todaysData.push(`${stockObj[date]['5. volume']}`);
+        break;
+    }   
+    const tableData = [{d1: todaysData[0], d2: todaysData[1], d3: todaysData[2], d4: todaysData[3], d5: todaysData[4]}]
+    return (
+      <div className="uk-position-center">
+        <Table caption={`Today's Stock Data for ${ticker}`} head={['Open', 'High', 'Low', 'Close', 'Volume']} body={tableData}/>
+      </div> 
+    )
+  } 
+}
+
+function mapStateToProps(state) {
+  return {
+    stockSymbol: state.stock.currentStock.stockSymbol,
+    stockData: state.stock.currentStock.stockData
+  }
+}
+
+export default connect(mapStateToProps, null)(StockTable); 
