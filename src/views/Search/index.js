0ab14e4e7e70c883830e5f75f0ffdc7d7fe74fc9
lizdenhup <lizdenhup@gmail.com>
2017-07-13 15:45:03 -0500

    formatting resp successfully

--- src/views/Search/index.js
+++ src/views/Search/index.js
@@ -30,9 +30,22 @@ class Search extends Component {
         </div>
       )
     } else if (this.props.hasFetchedData) {
+      const ticker = this.props.stockData['Meta Data']['2. Symbol']
+      const stockObj = [Object.values(this.props.stockData)[1]][0]
+      var todaysData = []   
+        for (var date in stockObj) {
+            todaysData.push(`${stockObj[date]['1. open']}`);
+            todaysData.push(`${stockObj[date]['2. high']}`);
+            todaysData.push(`${stockObj[date]['3. low']}`);
+            todaysData.push(`${stockObj[date]['4. close']}`);
+            todaysData.push(`${stockObj[date]['5. volume']}`);
+            break;
+        }   
+      //simply need to add these values to a table 
+      console.log(todaysData)
       return (
       <div>
-      {JSON.stringify(this.props.stockData)}
+      {ticker}
       </div> 
       )
     } else {
