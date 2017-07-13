bb5c928985bb821f43ffa5b00b526d03138ba023
lizdenhup <lizdenhup@gmail.com>
2017-07-13 18:04:53 -0500

    adding icons from uikit

--- src/views/StockTable/index.js
+++ src/views/StockTable/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import Table from 'react-uikit-table';
 import Panel from 'react-uikit-panel';
+import Icons from 'react-uikit-icons';
 
 class StockTable extends Component {
 
